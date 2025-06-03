56008d52d1aee46a815113a40f77bca4d3cfbe14
afer0093 <peanuts1909@gmail.com>
2025-06-03 16:16:46 +0900

    chore: update run_docker.sh

--- omnilrs.docker/run_docker.sh
+++ omnilrs.docker/run_docker.sh
@@ -5,7 +5,7 @@ docker run --name isaac-sim-omnilrs-container -it --gpus all -e "ACCEPT_EULA=Y" 
 -e DISPLAY \
 -e "PRIVACY_CONSENT=Y" \
 -v ${PWD}:/workspace/omnilrs \
--v ${PWD}/../code:/workspace/omnilrs/code \
+-v ${PWD}/../grad_research:/workspace/omnilrs/code \
 -v /usr/share/nvidia:/usr/share/nvidia:ro \
 -v ~/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
 -v ~/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
