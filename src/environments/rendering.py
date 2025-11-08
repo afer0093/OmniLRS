1687d84a8a874a74cfdc6eed53b13fe08379fef7
afer0093 <peanuts1909@gmail.com>
2025-11-08 20:57:14 +0900

    Revert "feat: reset RTX renderer before enabling real-time or path-tracing rendering"
    This reverts commit acebb3885af17600b9cb55cf59ec4a014fc63ded.

--- src/environments/rendering.py
+++ src/environments/rendering.py
@@ -11,19 +11,12 @@ import carb
 import omni
 
 from src.configurations.rendering_confs import FlaresConf, ChromaticAberrationsConf, MotionBlurConf
-from omni.kit.viewport.utility import get_active_viewport
 
 
 # ==============================================================================
 # Renderer Control
 # ==============================================================================
 
-def _reset_rtx_renderer():
-    vp = get_active_viewport()
-    # RTX レンダラを完全解放
-    vp.set_renderer_plugin(None)
-    # RTX レンダラを再ロード
-    vp.set_renderer_plugin("rtx")
 
 def enable_RTX_real_time(**kwargs) -> None:
     """
@@ -33,7 +26,6 @@ def enable_RTX_real_time(**kwargs) -> None:
         data (int, optional): Not used. Defaults to 0.
     """
 
-    _reset_rtx_renderer()
     action_registry = omni.kit.actions.core.get_action_registry()
     action = action_registry.get_action("omni.kit.viewport.actions", "set_renderer_rtx_realtime")
     action.execute()
@@ -47,7 +39,6 @@ def enable_RTX_interactive(*kwargs) -> None:
         data (int, optional): Not used. Defaults to 0.
     """
 
-    _reset_rtx_renderer()
     action_registry = omni.kit.actions.core.get_action_registry()
     action = action_registry.get_action("omni.kit.viewport.actions", "set_renderer_rtx_pathtracing")
     action.execute()
